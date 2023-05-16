8a569f1f9a98b8f318379548e07b18b2a235ce9c
Achmed Ali Al Yusa <101281644+Shijuuten@users.noreply.github.com>
2023-05-16 22:58:50 +0700

    test 2

---
+++ Main.py
@@ -0,0 +1,17 @@
+# Importing library
+import qrcode
+import numpy as np
+# Data to encode
+def trapesium(f, a, b, n):
+    h = float(b - a) / n
+    s = 0.0
+    s += f(a)/2.0
+    for i in range(1, n):
+        s += f(a + i*h)
+    s += f(b)/2.0
+    return s * h
+f = lambda x: (3*x**2)*(x**3+2)**2
+
+# ini adalah hasil perubahan dari code sebelumnya
+
+print(trapesium(f, 0, 2, 4))
